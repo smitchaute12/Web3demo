b593330e9418c5bad2eeb35f45679fbe244de755
smit chaute <smit.chaute@fibonalabs.com>
2022-03-22 12:20:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Greeter from "./BE/src/artifacts/contracts/Greeter.sol/Greeter.json";
+import { ethers } from "ethers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Please install MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      fetchGreetings();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchGreetings = async () => {
+    let contractAddress = "YOUR_CONTRACT_ADDRESS";
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install MetaMask!");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      contractAddress,
+      Greeter.abi,
+      provider
+    );
+
+    const greeting = await contract.greet();
+    console.log(greeting);
+  };
+
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
+  return <div></div>;
 }
-
-export default App;
